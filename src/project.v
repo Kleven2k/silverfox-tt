3c9b2e5b882a96d952aac0b2f7ca3a178166f23c
Fredrik Kleven <fredrikgkleven@gmail.com>
2026-09-20 18:50:12 +0000

    Regenerate project.v with real ISA core; add cocotb test verifying TX waveform periodicity against actual gnerated Verilog

--- src/project.v
+++ src/project.v
@@ -1,8 +1,8 @@
-module silverfox_core (
+module silverfox_isa (
     clk,
     rst_n,
-    ena,
     ui_in,
+    ena,
     uio_in,
     uo_out,
     uio_out,
@@ -11,39 +11,291 @@ module silverfox_core (
 
     input clk;
     input rst_n;
-    input ena;
     input [7:0] ui_in;
+    input ena;
     input [7:0] uio_in;
     output [7:0] uo_out;
     output [7:0] uio_out;
     output [7:0] uio_oe;
 
     wire [7:0] signal_const;
+    wire signal_const_2;
+    wire signal_select;
+    wire [2:0] signal_const_3;
+    wire signal_eq;
+    wire signal_mux;
+    wire signal_select_1;
+    wire signal_mux_1;
+    wire signal_mux_2;
+    wire signal_mux_3;
+    wire signal_mux_4;
+    wire [15:0] signal_const_5;
+    wire [15:0] signal_const_34;
+    wire [15:0] signal_const_35;
+    wire [15:0] signal_const_36;
+    wire [4:0] signal_const_37;
+    wire [4:0] signal_const_38;
+    wire [4:0] signal_add;
+    wire [9:0] signal_select_2;
+    wire [4:0] signal_select_3;
+    wire [4:0] signal_add_1;
+    wire [2:0] signal_const_40;
+    wire signal_eq_1;
+    wire signal_and;
     wire signal_wire;
     wire signal_not;
     wire signal_wire_1;
-    wire [7:0] signal_const_3;
-    wire [7:0] signal_add;
+    wire [2:0] signal_const_43;
+    wire signal_eq_2;
+    wire [7:0] signal_mux_5;
+    wire [7:0] signal_const_44;
+    wire [7:0] signal_sub;
+    wire signal_eq_3;
+    wire [7:0] signal_mux_6;
+    wire [7:0] signal_mux_7;
+    wire [6:0] signal_select_4;
+    wire [7:0] signal_cat;
+    wire [6:0] signal_select_5;
+    wire [7:0] signal_cat_1;
+    wire [7:0] signal_mux_8;
+    wire [7:0] signal_mux_9;
+    wire [7:0] signal_mux_10;
+    wire [7:0] signal_mux_11;
+    wire [7:0] signal_mux_12;
+    wire [7:0] signal_mux_13;
     wire [7:0] signal_wire_2;
     reg [7:0] signal_reg;
-    wire signal_wire_3;
-    wire [7:0] signal_mux;
+    wire signal_eq_4;
+    wire signal_not_1;
+    wire signal_eq_5;
+    wire signal_and_1;
+    wire [2:0] signal_const_48;
+    wire signal_eq_6;
+    wire [2:0] signal_select_6;
+    wire signal_eq_7;
+    wire signal_or;
+    wire signal_or_1;
+    wire signal_or_2;
+    wire [4:0] signal_mux_14;
+    wire [4:0] signal_add_2;
+    wire [7:0] signal_select_7;
+    wire signal_select_8;
+    wire [7:0] signal_wire_3;
+    wire signal_select_9;
+    wire signal_eq_8;
+    wire [4:0] signal_mux_15;
+    wire [4:0] signal_add_3;
+    wire [4:0] signal_add_4;
+    wire [4:0] signal_add_5;
+    wire [4:0] signal_add_6;
+    wire [2:0] signal_const_55;
+    wire signal_eq_9;
+    wire [4:0] signal_mux_16;
+    wire [2:0] signal_const_56;
+    wire signal_eq_10;
+    wire [4:0] signal_mux_17;
+    wire signal_eq_11;
+    wire [4:0] signal_mux_18;
+    wire signal_eq_12;
+    wire [4:0] signal_mux_19;
+    wire signal_eq_13;
+    wire [4:0] signal_mux_20;
+    wire [2:0] signal_const_60;
+    wire signal_eq_14;
+    wire [4:0] signal_mux_21;
+    wire [4:0] signal_mux_22;
+    wire [4:0] signal_wire_4;
+    reg [4:0] signal_reg_1;
+    reg [15:0] signal_mux_23;
+    wire [2:0] signal_select_10;
+    wire signal_eq_15;
+    wire signal_mux_24;
+    wire signal_wire_5;
+    reg signal_reg_2;
+    wire [6:0] signal_const_61;
+    wire [7:0] signal_cat_2;
     assign signal_const = 8'b00000000;
+    assign signal_const_2 = 1'b0;
+    assign signal_select = signal_select_7[0:0];
+    assign signal_const_3 = 3'b000;
+    assign signal_eq = signal_select_6 == signal_const_3;
+    assign signal_mux = signal_eq ? signal_select : signal_reg_2;
+    assign signal_select_1 = signal_reg[0:0];
+    assign signal_mux_1 = signal_eq_11 ? signal_select_1 : signal_reg_2;
+    assign signal_mux_2 = signal_eq_12 ? signal_reg_2 : signal_mux_1;
+    assign signal_mux_3 = signal_eq_13 ? signal_reg_2 : signal_mux_2;
+    assign signal_mux_4 = signal_eq_14 ? signal_reg_2 : signal_mux_3;
+    assign signal_const_5 = 16'b1110000000000000;
+    assign signal_const_34 = 16'b1000000000000000;
+    assign signal_const_35 = 16'b0000000000000000;
+    assign signal_const_36 = 16'b0000000000000001;
+    assign signal_const_37 = 5'b00000;
+    assign signal_const_38 = 5'b00001;
+    assign signal_add = signal_reg_1 + signal_const_38;
+    assign signal_select_2 = signal_mux_23[9:0];
+    assign signal_select_3 = signal_select_2[4:0];
+    assign signal_add_1 = signal_reg_1 + signal_const_38;
+    assign signal_const_40 = 3'b011;
+    assign signal_eq_1 = signal_select_6 == signal_const_40;
+    assign signal_and = signal_eq_1 & signal_select_9;
     assign signal_wire = rst_n;
     assign signal_not = ~ signal_wire;
     assign signal_wire_1 = clk;
-    assign signal_const_3 = 8'b00000001;
-    assign signal_add = signal_reg + signal_const_3;
-    assign signal_wire_2 = signal_add;
+    assign signal_const_43 = 3'b010;
+    assign signal_eq_2 = signal_select_6 == signal_const_43;
+    assign signal_mux_5 = signal_eq_2 ? signal_select_7 : signal_reg;
+    assign signal_const_44 = 8'b00000001;
+    assign signal_sub = signal_reg - signal_const_44;
+    assign signal_eq_3 = signal_select_6 == signal_const_43;
+    assign signal_mux_6 = signal_eq_3 ? signal_sub : signal_reg;
+    assign signal_mux_7 = signal_or_2 ? signal_mux_6 : signal_reg;
+    assign signal_select_4 = signal_reg[7:1];
+    assign signal_cat = { signal_select_9,
+                          signal_select_4 };
+    assign signal_select_5 = signal_reg[7:1];
+    assign signal_cat_1 = { signal_const_2,
+                            signal_select_5 };
+    assign signal_mux_8 = signal_eq_10 ? signal_wire_3 : signal_reg;
+    assign signal_mux_9 = signal_eq_11 ? signal_cat_1 : signal_mux_8;
+    assign signal_mux_10 = signal_eq_12 ? signal_cat : signal_mux_9;
+    assign signal_mux_11 = signal_eq_13 ? signal_reg : signal_mux_10;
+    assign signal_mux_12 = signal_eq_14 ? signal_mux_7 : signal_mux_11;
+    assign signal_mux_13 = signal_eq_15 ? signal_mux_5 : signal_mux_12;
+    assign signal_wire_2 = signal_mux_13;
     always @(posedge signal_wire_1) begin
         if (signal_not)
             signal_reg <= signal_const;
         else
             signal_reg <= signal_wire_2;
     end
-    assign signal_wire_3 = ena;
-    assign signal_mux = signal_wire_3 ? signal_reg : signal_const;
-    assign uo_out = signal_mux;
+    assign signal_eq_4 = signal_reg == signal_const;
+    assign signal_not_1 = ~ signal_eq_4;
+    assign signal_eq_5 = signal_select_6 == signal_const_43;
+    assign signal_and_1 = signal_eq_5 & signal_not_1;
+    assign signal_const_48 = 3'b001;
+    assign signal_eq_6 = signal_select_6 == signal_const_48;
+    assign signal_select_6 = signal_mux_23[12:10];
+    assign signal_eq_7 = signal_select_6 == signal_const_3;
+    assign signal_or = signal_eq_7 | signal_eq_6;
+    assign signal_or_1 = signal_or | signal_and_1;
+    assign signal_or_2 = signal_or_1 | signal_and;
+    assign signal_mux_14 = signal_or_2 ? signal_select_3 : signal_add_1;
+    assign signal_add_2 = signal_reg_1 + signal_const_38;
+    assign signal_select_7 = signal_mux_23[7:0];
+    assign signal_select_8 = signal_select_7[0:0];
+    assign signal_wire_3 = ui_in;
+    assign signal_select_9 = signal_wire_3[0:0];
+    assign signal_eq_8 = signal_select_9 == signal_select_8;
+    assign signal_mux_15 = signal_eq_8 ? signal_add_2 : signal_reg_1;
+    assign signal_add_3 = signal_reg_1 + signal_const_38;
+    assign signal_add_4 = signal_reg_1 + signal_const_38;
+    assign signal_add_5 = signal_reg_1 + signal_const_38;
+    assign signal_add_6 = signal_reg_1 + signal_const_38;
+    assign signal_const_55 = 3'b111;
+    assign signal_eq_9 = signal_select_10 == signal_const_55;
+    assign signal_mux_16 = signal_eq_9 ? signal_reg_1 : signal_add_6;
+    assign signal_const_56 = 3'b101;
+    assign signal_eq_10 = signal_select_10 == signal_const_56;
+    assign signal_mux_17 = signal_eq_10 ? signal_add_5 : signal_mux_16;
+    assign signal_eq_11 = signal_select_10 == signal_const_40;
+    assign signal_mux_18 = signal_eq_11 ? signal_add_4 : signal_mux_17;
+    assign signal_eq_12 = signal_select_10 == signal_const_43;
+    assign signal_mux_19 = signal_eq_12 ? signal_add_3 : signal_mux_18;
+    assign signal_eq_13 = signal_select_10 == signal_const_48;
+    assign signal_mux_20 = signal_eq_13 ? signal_mux_15 : signal_mux_19;
+    assign signal_const_60 = 3'b100;
+    assign signal_eq_14 = signal_select_10 == signal_const_60;
+    assign signal_mux_21 = signal_eq_14 ? signal_mux_14 : signal_mux_20;
+    assign signal_mux_22 = signal_eq_15 ? signal_add : signal_mux_21;
+    assign signal_wire_4 = signal_mux_22;
+    always @(posedge signal_wire_1) begin
+        if (signal_not)
+            signal_reg_1 <= signal_const_37;
+        else
+            signal_reg_1 <= signal_wire_4;
+    end
+    always @* begin
+        case (signal_reg_1)
+        0:
+            signal_mux_23 <= signal_const_36;
+        1:
+            signal_mux_23 <= signal_const_35;
+        2:
+            signal_mux_23 <= signal_const_34;
+        3:
+            signal_mux_23 <= signal_const_5;
+        4:
+            signal_mux_23 <= signal_const_5;
+        5:
+            signal_mux_23 <= signal_const_5;
+        6:
+            signal_mux_23 <= signal_const_5;
+        7:
+            signal_mux_23 <= signal_const_5;
+        8:
+            signal_mux_23 <= signal_const_5;
+        9:
+            signal_mux_23 <= signal_const_5;
+        10:
+            signal_mux_23 <= signal_const_5;
+        11:
+            signal_mux_23 <= signal_const_5;
+        12:
+            signal_mux_23 <= signal_const_5;
+        13:
+            signal_mux_23 <= signal_const_5;
+        14:
+            signal_mux_23 <= signal_const_5;
+        15:
+            signal_mux_23 <= signal_const_5;
+        16:
+            signal_mux_23 <= signal_const_5;
+        17:
+            signal_mux_23 <= signal_const_5;
+        18:
+            signal_mux_23 <= signal_const_5;
+        19:
+            signal_mux_23 <= signal_const_5;
+        20:
+            signal_mux_23 <= signal_const_5;
+        21:
+            signal_mux_23 <= signal_const_5;
+        22:
+            signal_mux_23 <= signal_const_5;
+        23:
+            signal_mux_23 <= signal_const_5;
+        24:
+            signal_mux_23 <= signal_const_5;
+        25:
+            signal_mux_23 <= signal_const_5;
+        26:
+            signal_mux_23 <= signal_const_5;
+        27:
+            signal_mux_23 <= signal_const_5;
+        28:
+            signal_mux_23 <= signal_const_5;
+        29:
+            signal_mux_23 <= signal_const_5;
+        30:
+            signal_mux_23 <= signal_const_5;
+        default:
+            signal_mux_23 <= signal_const_5;
+        endcase
+    end
+    assign signal_select_10 = signal_mux_23[15:13];
+    assign signal_eq_15 = signal_select_10 == signal_const_3;
+    assign signal_mux_24 = signal_eq_15 ? signal_mux : signal_mux_4;
+    assign signal_wire_5 = signal_mux_24;
+    always @(posedge signal_wire_1) begin
+        if (signal_not)
+            signal_reg_2 <= signal_const_2;
+        else
+            signal_reg_2 <= signal_wire_5;
+    end
+    assign signal_const_61 = 7'b0000000;
+    assign signal_cat_2 = { signal_const_61,
+                            signal_reg_2 };
+    assign uo_out = signal_cat_2;
     assign uio_out = signal_const;
     assign uio_oe = signal_const;
 
@@ -84,8 +336,8 @@ module tt_um_silverfox_kleven2k (
     assign signal_wire_2 = ena;
     assign signal_wire_3 = rst_n;
     assign signal_wire_4 = clk;
-    silverfox_core
-        silverfox_core
+    silverfox_isa
+        silverfox_isa
         ( .clk(signal_wire_4),
           .rst_n(signal_wire_3),
           .ena(signal_wire_2),
--- test/test.py
+++ test/test.py
@@ -1,20 +1,12 @@
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
+# SPDX-FileCopyrightText: © 2026 Fredrik
 # SPDX-License-Identifier: Apache-2.0
 
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
+from cocotb.triggers import ClockCycles, RisingEdge
 
-
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
-
-    # Reset
+# --- Helpers ---
+async def reset(dut):
     dut._log.info("Reset")
     dut.ena.value = 1
     dut.ui_in.value = 0
@@ -23,18 +15,55 @@ async def test_project(dut):
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
 
-    dut._log.info("Test project behavior")
+async def start_clock(dut):
+    """Set the clock period to 10 ns (100 MHz)."""
+    cocotb.start_soon(Clock(dut.clk, 10, unit="ns").start())
 
-    # Set the input values you want to test
-    dut.ui_in.value = 20
-    dut.uio_in.value = 30
+@cocotb.test()
+async def test_project(dut):
+    """Verify the default program (SET tx=1 / SET tx=0 / JMP always->0)
+    produces a periodic TX waveform: one high cycle out of every three,
+    repeating indefinitely. This mirrors the Hardcaml-side SET/JMP test
+    in hardcaml/test/isa_test.ml, but runs against the actual generated
+    Verilog (src/project.v) via a real HDL simulator, per Tiny Tapeout's
+    required test flow -- rather than pinning an exact absolute cycle
+    sequence (which proved fragile to simulator-specific timing
+    differences during Hardcaml-side test development), this checks the
+    periodic *shape* of the waveform, which is what actually matters.
+    """
+    dut._log.info("Start")
 
-    # Wait for one clock cycle to see the output values
-    await ClockCycles(dut.clk, 1)
+    await start_clock(dut)
+    await reset(dut)
 
-    # The following assersion is just an example of how to check the output values.
-    # Change it to match the actual expected output of your module:
-    assert dut.uo_out.value == 50
+    dut._log.info("Sample TX (uo_out bit 0) over multiple periods")
 
-    # Keep testing the module by changing the input values, waiting for
-    # one or more clock cycles, and asserting the expected output values.
+    num_cycles = 12  # 4 full periods of the 3-instruction toggle loop
+    tx_trace = []
+    for _ in range(num_cycles):
+        await ClockCycles(dut.clk, 1)
+        tx_trace.append(int(dut.uo_out.value) & 1)
+
+    dut._log.info(f"tx_trace = {tx_trace}")
+
+    # Split into consecutive groups of 3 and check every group is
+    # identical -- this proves the waveform is genuinely periodic with
+    # period 3, without pinning which absolute cycle the pattern starts
+    # on (that phase can differ between simulators/harnesses).
+    period = 3
+    groups = [tx_trace[i:i + period] for i in range(0, len(tx_trace), period)]
+    first_group = groups[0]
+    for group in groups[1:]:
+        assert group == first_group, (
+            f"TX waveform is not periodic with period {period}: "
+            f"expected every group to equal {first_group}, got {tx_trace}"
+        )
+
+    # Within one period, exactly one cycle should be high (the SET tx=1
+    # instruction), matching the program's structure.
+    assert sum(first_group) == 1, (
+        f"Expected exactly one high cycle per period of {period}, "
+        f"got {first_group} (full trace: {tx_trace})"
+    )
+
+    dut._log.info("TX waveform confirmed periodic with correct duty cycle")
